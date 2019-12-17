820645e5e18a2f439ab5bafec92d402a0409426e
Aditya Tyagi <aditya.tyagi@ebabu.co>
2019-12-18 01:26:09 +0530

    SEO friendly Angular Application

--- src/app/home/pages/about-us/about-us.component.ts
+++ src/app/home/pages/about-us/about-us.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+// importing service
+import { Meta, Title } from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-about-us',
-  templateUrl: './about-us.component.html',
-  styleUrls: ['./about-us.component.scss']
+  selector: "app-about-us",
+  templateUrl: "./about-us.component.html",
+  styleUrls: ["./about-us.component.scss"]
 })
 export class AboutUsComponent implements OnInit {
+  constructor(private _meta: Meta, private _title: Title) {
+    this._meta.updateTag({ name: "author", content: "Aditya Tyagi" });
+    this._meta.updateTag({
+      name: "description",
+      content:
+        "Aditya Tyagi is trying his best to describe himself in this description"
+    });
+    this._meta.updateTag({
+      name: "keywords",
+      content: "About Aditya Tyagi, Meta Service, SEO, Angular"
+    });
 
-  constructor() { }
-
-  ngOnInit() {
+    // adding title
+    this._title.setTitle("About Us | Scalable Angular App");
   }
 
+  ngOnInit() {}
 }
--- src/app/home/pages/contact-us/contact-us.component.ts
+++ src/app/home/pages/contact-us/contact-us.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Meta, Title } from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-contact-us',
-  templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.scss']
+  selector: "app-contact-us",
+  templateUrl: "./contact-us.component.html",
+  styleUrls: ["./contact-us.component.scss"]
 })
 export class ContactUsComponent implements OnInit {
+  constructor(private _meta: Meta, private _title: Title) {
+    this._meta.updateTag({ name: "author", content: "Aditya Tyagi" });
+    this._meta.updateTag({
+      name: "description",
+      content: "Reach out to Aditya via adityatyagi.com"
+    });
+    this._meta.updateTag({
+      name: "keywords",
+      content: "Contact Aditya Tyagi, Meta Service, SEO, Angular"
+    });
 
-  constructor() { }
-
-  ngOnInit() {
+    // adding title
+    this._title.setTitle("Contact Us | Scalable Angular App");
   }
 
+  ngOnInit() {}
 }
