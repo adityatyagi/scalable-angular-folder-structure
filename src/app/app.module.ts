6d77f209e6183dd864b95f253a4ab6730d5ce23a
adityatyagi <tyagi.aditya844747@gmail.com>
2019-12-30 12:58:13 +0530

    minor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, ServiceWorkerModule.register('./ngsw-worker.js', { enabled: environment.production })],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [],
   bootstrap: [AppComponent]
 })
